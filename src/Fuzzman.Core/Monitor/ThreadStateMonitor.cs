9451ee210fb3002c7fcb32f99dd71e27810da724
dev-zzo <gauri@tut.by>
2013-07-29 15:29:51 +0200

    Added some links for reference.

---
+++ src/Fuzzman.Core/Monitor/ThreadStateMonitor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fuzzman.Core.Monitor
+{
+    // See http://undocumented.ntinternals.net/UserMode/Undocumented%20Functions/System%20Information/Structures/SYSTEM_PROCESS_INFORMATION.html
+    public class ThreadStateMonitor
+    {
+    }
+}
