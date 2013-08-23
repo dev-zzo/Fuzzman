0fd888e5d9b4be7eaf87290a25a798088a168228
dev-zzo <gauri@tut.by>
2013-08-23 16:39:43 +0200

    Fault reports are hashed now.

--- src/Fuzzman.Agent/FaultReport.cs
+++ src/Fuzzman.Agent/FaultReport.cs
@@ -5,6 +5,12 @@ namespace Fuzzman.Agent
 {
     public class FaultReport
     {
+        public int TimesOccurred;
+
+        public override bool Equals(object obj)
+        {
+            return this.GetHashCode() == obj.GetHashCode();
+        }
     }
 
     public class ExceptionFaultReport : FaultReport
@@ -16,6 +22,13 @@ namespace Fuzzman.Agent
         public string Location;
 
         public CONTEXT Context;
+
+        public override int GetHashCode()
+        {
+            int hash = (int)this.ExceptionCode;
+            hash ^= this.Location != null ? this.Location.GetHashCode() : (int)this.OffendingVA;
+            return hash;
+        }
     }
 
     public class AccessViolationFaultReport : ExceptionFaultReport
@@ -23,5 +36,10 @@ namespace Fuzzman.Agent
         public string AccessType;
 
         public IntPtr TargetVA;
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() ^ this.AccessType.GetHashCode();
+        }
     }
 }
